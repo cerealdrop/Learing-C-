b9bcfd364aa1c74bec6d809d8cf170f62716589a
cerealdrop <raphdv15@gmail.com>
2017-07-28 20:16:18 -0400

    dice game added

---
+++ c#/dice_game/dice_game/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace dice_game
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Random randomnumber = new Random();
+			int numberofattempts = 0;
+
+			int attempts = 0;
+
+			while (attempts!=6)
+			{
+				attempts = randomnumber.Next(1, 7);
+				numberofattempts++;
+				Console.WriteLine("You rolled a: "+attempts);
+				if (attempts != 6)
+				{
+					Console.WriteLine("You did not roll a six i will try again");
+				}
+				else { 
+					Console.WriteLine("great job");
+				}
+			}
+			Console.WriteLine("You attempted this game "+numberofattempts+" times");
+		}
+	}
+}
